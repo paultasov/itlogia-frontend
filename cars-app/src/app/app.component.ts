d710f575c511b5d4c8e67db23b67d1ea4684ead9
paultan <hipaultan@gmail.com>
2023-01-15 05:26:43 +0300

    Знакомство с Angular

---
+++ cars-app/src/app/app.component.ts
@@ -0,0 +1,74 @@
+import { Component } from '@angular/core';
+import {FormBuilder, Validators} from "@angular/forms";
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  priceForm = this.fb.group({
+    name: ['', Validators.required],
+    phone: ['', Validators.required],
+    car: ['', Validators.required],
+  });
+
+  carsData = [
+    {
+      image: '1.png',
+      name: 'Lamborghini Huracan Spyder',
+      gear: 'полный',
+      engine: 5.2,
+      places: 2,
+    },
+    {
+      image: '2.png',
+      name: 'Chevrolet Corvette',
+      gear: 'полный',
+      engine: 6.2,
+      places: 2,
+    },
+    {
+      image: '3.png',
+      name: 'Ferrari California',
+      gear: 'полный',
+      engine: 3.9,
+      places: 4,
+    },
+    {
+      image: '4.png',
+      name: 'Lamborghini Urus',
+      gear: 'полный',
+      engine: 4,
+      places: 5,
+    },
+    {
+      image: '5.png',
+      name: 'Audi R8',
+      gear: 'полный',
+      engine: 5.2,
+      places: 2,
+    },
+    {
+      image: '6.png',
+      name: 'Chevrolet Camaro',
+      gear: 'полный',
+      engine: 2.0,
+      places: 4,
+    }
+  ];
+
+  constructor(private fb: FormBuilder) {
+  }
+
+  goScroll(target: HTMLElement) {
+    target.scrollIntoView({behavior: 'smooth'});
+  }
+
+  onSubmit() {
+    if (this.priceForm.valid) {
+      alert('Спасибо за заявку, мы свяжемся с вами в ближайшее время!');
+      this.priceForm.reset();
+    }
+  }
+}
