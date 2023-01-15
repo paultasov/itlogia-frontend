afe5f04e6d834fbb495a4ac86ef2d7bb8bc8f548
paultan <hipaultan@gmail.com>
2023-01-15 13:13:41 +0300

    Выкатываем на продакшен

--- cars-app/src/app/app.component.ts
+++ cars-app/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import {Component, HostListener} from '@angular/core';
 import {FormBuilder, Validators} from "@angular/forms";
+import {AppService} from "./app.service";
 
 @Component({
   selector: 'app-root',
@@ -13,52 +14,13 @@ export class AppComponent {
     car: ['', Validators.required],
   });
 
-  carsData = [
-    {
-      image: '1.png',
-      name: 'Lamborghini Huracan Spyder',
-      gear: 'полный',
-      engine: 5.2,
-      places: 2,
-    },
-    {
-      image: '2.png',
-      name: 'Chevrolet Corvette',
-      gear: 'полный',
-      engine: 6.2,
-      places: 2,
-    },
-    {
-      image: '3.png',
-      name: 'Ferrari California',
-      gear: 'полный',
-      engine: 3.9,
-      places: 4,
-    },
-    {
-      image: '4.png',
-      name: 'Lamborghini Urus',
-      gear: 'полный',
-      engine: 4,
-      places: 5,
-    },
-    {
-      image: '5.png',
-      name: 'Audi R8',
-      gear: 'полный',
-      engine: 5.2,
-      places: 2,
-    },
-    {
-      image: '6.png',
-      name: 'Chevrolet Camaro',
-      gear: 'полный',
-      engine: 2.0,
-      places: 4,
-    }
-  ];
+  carsData: any;
 
-  constructor(private fb: FormBuilder) {
+  constructor(private fb: FormBuilder, private appService: AppService) {
+  }
+
+  ngOnInit() {
+    this.appService.getData().subscribe(carsData => this.carsData = carsData);
   }
 
   goScroll(target: HTMLElement, car?: any) {
@@ -82,8 +44,18 @@ export class AppComponent {
 
   onSubmit() {
     if (this.priceForm.valid) {
-      alert('Спасибо за заявку, мы свяжемся с вами в ближайшее время!');
-      this.priceForm.reset();
+      this.appService.sendQuery(this.priceForm.value)
+        .subscribe(
+          {
+            next: (response: any) => {
+              alert(response.message);
+              this.priceForm.reset();
+            },
+            error: (response) => {
+              alert(response.error.message);
+            }
+          }
+        );
     }
   }
 }
